96c6a65a3f78396e7f0d63360c7d45f89d84ae99
Alvaro Rubio <aljrubior@gmail.com>
2022-03-04 11:16:38 -0300

    implemented application deployed notification

---
+++ src/runtime/messages/notifications/putDomainDeploymentDeployedNotification.go
@@ -0,0 +1,33 @@
+package notifications
+
+import "strings"
+
+func NewPutDomainDeploymentDeployedNotification(serverId, contextId string) *PutDomainDeploymentDeployedNotification {
+	return &PutDomainDeploymentDeployedNotification{
+		serverId:  serverId,
+		contextId: contextId,
+	}
+}
+
+type PutDomainDeploymentDeployedNotification struct {
+	serverId  string
+	contextId string
+	message   string
+}
+
+func (this PutDomainDeploymentDeployedNotification) CreateNotification() string {
+	message := `PUT domains/default/deployment HTTP/1.1
+Content-Type: application/json
+X-ANYPNT-SERVER-ID: {{serverId}}
+X-ANYPNT-CTX-ID: {{contextId}}
+X-ANYPNT-AGENT-VERSION: 2.4.28-SNAPSHOT
+Content-Length: 80
+Content-Encoding: UTF-8
+
+{"status":"DEPLOYED","message":"","domain":{"name":"default","applications":[]}}`
+
+	message = strings.Replace(message, SERVER_ID_PATTERN, this.serverId, 1)
+	message = strings.Replace(message, CONTEXT_ID_PATTERN, this.contextId, 1)
+
+	return message
+}
